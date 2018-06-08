9405d4d0e856c27b9a738317f41c7b1eb4f396b9
Yanish Ramboucs <yanishrambocus@gmail.com>
2018-06-08 12:19:10 -0500

    working again

--- server.js
+++ server.js
@@ -2,31 +2,10 @@ var http = require('http'),
 	url = require('url'),
 	fs = require('fs');
 
-function css(req, res){
-	if (request.url === '/style.css'){
-		response.writeHead(200, {'Content-Type' : 'text/css'});
-		var fileContents = fs.readFileSync('./views/style.css', {encoding: 'utf8'});
-		response.write(fileContents);
-	}
-}
-
-
-var server = http.createServer(function (req, res){
-	router.css(req, res);
-	router.home(req, res);
-	router.user(req, res);
-
-});
-
 http.createServer(function (req, res){
 	fs.readFile('index.html', function(err, data){
 		res.writeHead(200, {'Content-Type' : 'text/html'});
 		res.write(data);
 		res.end();
 	});
-});
-
-
-
-
-server.listen(8080);
+}).listen(8080);
