f18b683ca3dabe5eb7aefb886c14e8c9ebd69639
Yanish Ramboucs <yanishrambocus@gmail.com>
2018-06-08 13:55:22 -0500

    updated and working

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 var http = require('http'),
 	url = require('url'),
+	connect = require('connect'),
+	serveStatic = require('serve-static'),
 	fs = require('fs');
 
-http.createServer(function (req, res){
+/*http.createServer(function (req, res){
 	fs.readFile('index.html', function(err, data){
 		res.writeHead(200, {'Content-Type' : 'text/html'});
 		res.write(data);
 		res.end();
 	});
-}).listen(8080);
+
+}).listen(8080);*/
+
+connect().use(serveStatic(__dirname)).listen(8080, function(){
+	console.log('Server running on 8080...');
+});
