2feaf34493f64b3bb0e6d94a2d787d997573818f
Yanish Ramboucs <yanishrambocus@gmail.com>
2018-06-13 16:29:14 -0500

    email submission
    You can now submit your email indicating your interest in the project. The email goes to data/emails.txt

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express'),
-	path = require('path');
+	path = require('path')
+	body = require('body-parser'),
+	fs = require('fs');
 
 const app = new express();
 
 app.use(express.static(path.join(__dirname + '/public')));
+app.use(body.urlencoded({ extended : true}));
+
+//To handle email form
+app.post('/', (req, res) => {
+	res.send("Your email is " + req.body.email);
+	fs.appendFile(path.join(__dirname)+'/data/emails.txt', req.body.email, () => {
+		console.log("Saved");
+	});
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
